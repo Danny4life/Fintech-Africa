856be053ecea04f6617c46ce3f17ad8c763fba9c
Segun Osiki <osiki.segun@gmail.com>
2022-09-08 00:28:49 +0100

    first commit

---
+++ src/pages/dashboard/components/home/Home.jsx
@@ -0,0 +1,15 @@
+import Topbar from '../components/topbar/Topbar'
+import ViewWallet from '../components/Viewwallet'
+import React from 'react'
+import Transaction from '../components/transaction/transaction'
+const Home = () => {
+  return (
+    <div>
+        <Topbar />
+        <ViewWallet />
+        <Transaction />
+    </div>
+  );
+}
+
+export default Home
